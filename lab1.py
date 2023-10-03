74386d9d0245dce357bb05660fd941ae7dc0fa86
Ilya Leon <ilialeonenko06@gmail.com>
2023-10-03 14:42:45 +0300

    commit7

--- lab1.py
+++ lab1.py
@@ -34,7 +34,7 @@ def MMG(b_array,c_array,k):
 
 a = 29791  
 x0 = 29791
-c = 17
+c = 0
 m = 2**31
 k = 128
 n = 1000
@@ -44,27 +44,25 @@ lcg_sample = LCG(a,x0,c,m,n)
 mmg_sample = []
 for i in range(n):
     mmg_sample.append(MMG(mcg_sample,lcg_sample,k))
-print(mcg_sample,'\n')
-print(lcg_sample,'\n')
-print(mmg_sample,'\n')
-print('|', mcg_sample.center(10), '|')
-# Проверка равномерности МКМ
-# mkm_stat, mkm_p = stats.kstest(mkm_sample,'uniform', N=n)
-# print('Статистика К-С для МКМ:', mkm_stat)
-# print('p-значение К-С для МКМ:', mkm_p)
 
-# mkm_chi2, mkm_p = stats.chisquare(mkm_sample)
-# print('Статистика хи-квадрат для МКМ:', mkm_chi2)
-# print('p-значение хи-квадрат для МКМ:', mkm_p)
+
+# Проверка равномерности МКМ
+mcm_stat, mcm_p = stats.kstest(mcg_sample, stats.uniform.cdf)
+print('Статистика К-С для МКМ:', mcm_stat)
+print('p-значение К-С для МКМ:', mcm_p)
+
+mcm_chi2, mcm_p = stats.chisquare(mcg_sample)
+print('Статистика хи-квадрат для МКМ:', mcm_chi2)
+print('p-значение хи-квадрат для МКМ:', mcm_p)
 
 # # Проверка равномерности ММ
-# mm_stat, mm_p = stats.kstest(mm_sample,'uniform', N=n)
-# print('Статистика К-С для ММ:', mm_stat) 
-# print('p-значение К-С для ММ:', mm_p)
+mm_stat, mm_p = stats.kstest(mmg_sample,'uniform', N=n)
+print('Статистика К-С для ММ:', mm_stat) 
+print('p-значение К-С для ММ:', mm_p)
 
-# mm_chi2, mm_p = stats.chisquare(mm_sample)
-# print('Статистика хи-квадрат для ММ:', mm_chi2)
-# print('p-значение хи-квадрат для ММ:', mm_p)
+mm_chi2, mm_p = stats.chisquare(mmg_sample)
+print('Статистика хи-квадрат для ММ:', mm_chi2)
+print('p-значение хи-квадрат для ММ:', mm_p)
 
 # Диаграмма рассеяния
 plt.subplot (2, 2, 1)
