4fb1a6bb215a740c4812790c3987fb43e45a687e
Ilya Leon <ilialeonenko06@gmail.com>
2023-11-21 14:35:33 +0300

    lab5_final

--- lab5.py
+++ lab5.py
@@ -1,13 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def monte_carlo_solution(n,N,m,a,f):
-    h = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+def mc_simulation(n,N,m,a,f,h):
     pi = np.array([1/n, 1/n, 1/n])
     p = np.array([[1/n, 1/n, 1/n], [1/n, 1/n, 1/n], [1/n, 1/n, 1/n]])
     i = np.zeros(N+1, dtype=int)
-    Q = np.zeros((N+1, n))  
-    ksi = np.zeros((m, n))  
+    Q = np.zeros(N+1)
+    ksi = np.zeros(m)
+
 
     for j in range(m):
         alpha = np.random.rand()
@@ -41,9 +41,55 @@ def monte_carlo_solution(n,N,m,a,f):
         for k in range(N+1):
             ksi[j] += Q[k] * f[i[k]]
 
-    return np.mean(ksi, axis=0)
+    return np.mean(ksi)
 
 
+
+def monte_carlo_solution(n,N,m,a,f):
+    # h = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    # pi = np.array([1/n, 1/n, 1/n])
+    # p = np.array([[1/n, 1/n, 1/n], [1/n, 1/n, 1/n], [1/n, 1/n, 1/n]])
+    # i = np.zeros(N+1, dtype=int)
+    # Q = np.zeros((N+1, n))  
+    # ksi = np.zeros((m, n))  
+
+    # for j in range(m):
+    #     alpha = np.random.rand()
+    #     if alpha < pi[0]:
+    #         i[0] = 0
+    #     elif alpha < pi[0] + pi[1]:
+    #         i[0] = 1
+    #     else:
+    #         i[0] = 2
+
+    #     for k in range(1, N+1):
+    #         alpha = np.random.rand()
+    #         if alpha < p[i[k-1]][0]:
+    #             i[k] = 0
+    #         elif alpha < p[i[k-1]][0] + p[i[k-1]][1]:
+    #             i[k] = 1
+    #         else:
+    #             i[k] = 2
+
+    #     if pi[i[0]] > 0:
+    #         Q[0] = h[i[0]] / pi[i[0]]
+    #     else:
+    #         Q[0] = 0
+
+    #     for k in range(1, N+1):
+    #         if p[i[k-1]][i[k]] > 0:
+    #             Q[k] = Q[k-1] * a[i[k-1]][i[k]] / p[i[k-1]][i[k]]
+    #         else:
+    #             Q[k] = 0
+
+    #     for k in range(N+1):
+    #         ksi[j] += Q[k] * f[i[k]]
+
+    # return np.mean(ksi, axis=0)
+    return [mc_simulation(n,N,m,a,f,[1,0,0]),mc_simulation(n,N,m,a,f,[0,1,0]),mc_simulation(n,N,m,a,f,[0,0,1])]
+
+A_v7 = np.array([[1.2, -0.4, 0.3], [0.1, 0.7, -0.2], [-0.4, 0.0, 1.4]])
+a_v7 = np.array([[-0.2, 1.4, 0.7], [0.9, 0.3, 1.2], [1.4, 1.0, -0.4]])
 A = np.array([[0.5, 0, 0], [0, 0.3, 0], [0, 0, 0.7]])
 a = np.array([[0.5, 0, 0], [0, 0.7, 0], [0, 0, 0.3]])
 f = np.array([1, 2, -2])
@@ -51,22 +97,40 @@ size = len(f)
 exact_solution = np.linalg.solve(A, f)
 print(f'exact_solution = {exact_solution}')
 
-num_chains = 100
-chain_lengths = np.arange(10000, 20000, 1000)
-errors = []
-for chain_length in chain_lengths:
-    monte_carlo_solve = monte_carlo_solution(size,num_chains,chain_length,a,f)
+num_chains1 = 500
+chain_lengths1 = np.arange(1000, 10000, 100)
+errors1 = []
+for chain_length in chain_lengths1:
+    monte_carlo_solve = monte_carlo_solution(size,num_chains1,chain_length,a,f)
     print(monte_carlo_solve)
     error = np.linalg.norm(monte_carlo_solve - exact_solution)
-    errors.append(error)
+    errors1.append(error)
 
-# Построение графика
+num_chains2 = np.arange(1000, 10000, 100)
+chain_lengths2 = 1000
+errors2 = []
+for num_chain in num_chains2:
+    monte_carlo_solve = monte_carlo_solution(size,num_chain,chain_lengths2,a,f)
+    print(monte_carlo_solve)
+    error = np.linalg.norm(monte_carlo_solve - exact_solution)
+    errors2.append(error)
+    
+
 plt.figure(figsize=(10, 6))
-plt.plot(chain_lengths, errors, marker='o')
+plt.plot(chain_lengths1, errors1, marker='o')
 plt.title('Зависимость точности решения от длины цепи Маркова')
 plt.xlabel('Длина цепи Маркова')
 plt.ylabel('Ошибка')
 plt.grid(True)
 plt.show()
+ 
+plt.figure(figsize=(10, 6))
+plt.plot(num_chains2, errors2, marker='o')
+plt.title('Зависимость точности решения от кол-во цепей Маркова')
+plt.xlabel('Длина цепи Маркова')
+plt.ylabel('Ошибка')
+plt.grid(True)
+plt.show()
 
-
+monte_carlo_solve = monte_carlo_solution(size,5000,10000,a,f)
+print(f'monte_carlo_solve = {monte_carlo_solve}')
