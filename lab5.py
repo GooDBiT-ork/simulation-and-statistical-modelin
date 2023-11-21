82963dd29fea10ec9c63a5d71ade0744f56d1025
Ilya Leon <ilialeonenko06@gmail.com>
2023-11-21 11:47:18 +0300

    lab5

--- lab5.py
+++ lab5.py
@@ -1,60 +1,72 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.linalg import solve
 
-# Ваша система уравнений
-A = np.array([[1.2, -0.4, 0.3], [0.1, 0.7, -0.2], [-0.4, 0.0, 1.4]])
+def monte_carlo_solution(n,N,m,a,f):
+    h = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+    pi = np.array([1/n, 1/n, 1/n])
+    p = np.array([[1/n, 1/n, 1/n], [1/n, 1/n, 1/n], [1/n, 1/n, 1/n]])
+    i = np.zeros(N+1, dtype=int)
+    Q = np.zeros((N+1, n))  
+    ksi = np.zeros((m, n))  
+
+    for j in range(m):
+        alpha = np.random.rand()
+        if alpha < pi[0]:
+            i[0] = 0
+        elif alpha < pi[0] + pi[1]:
+            i[0] = 1
+        else:
+            i[0] = 2
+
+        for k in range(1, N+1):
+            alpha = np.random.rand()
+            if alpha < p[i[k-1]][0]:
+                i[k] = 0
+            elif alpha < p[i[k-1]][0] + p[i[k-1]][1]:
+                i[k] = 1
+            else:
+                i[k] = 2
+
+        if pi[i[0]] > 0:
+            Q[0] = h[i[0]] / pi[i[0]]
+        else:
+            Q[0] = 0
+
+        for k in range(1, N+1):
+            if p[i[k-1]][i[k]] > 0:
+                Q[k] = Q[k-1] * a[i[k-1]][i[k]] / p[i[k-1]][i[k]]
+            else:
+                Q[k] = 0
+
+        for k in range(N+1):
+            ksi[j] += Q[k] * f[i[k]]
+
+    return np.mean(ksi, axis=0)
+
+
+A = np.array([[0.5, 0, 0], [0, 0.3, 0], [0, 0, 0.7]])
+a = np.array([[0.5, 0, 0], [0, 0.7, 0], [0, 0, 0.3]])
 f = np.array([1, 2, -2])
+size = len(f)
+exact_solution = np.linalg.solve(A, f)
+print(f'exact_solution = {exact_solution}')
 
-# Решение системы уравнений с помощью встроенной функции
-sol_scipy = solve(A, f)
-
-# Метод Монте-Карло
-def monte_carlo(A, f, N_chain, N_iter):
-    n = len(f)
-    x = np.zeros(n)
-    Q = np.tril(A)
-    R = A - Q
-    for _ in range(N_chain):
-        x_temp = np.zeros(n)
-        for _ in range(N_iter):
-            x_temp = np.dot(np.linalg.inv(Q), f - np.dot(R, x_temp))
-        x += x_temp
-    return x / N_chain
-
-# Параметры для метода Монте-Карло
-N_chain = 100
-N_iter = 1000
-
-# Решение системы уравнений методом Монте-Карло
-sol_mc = monte_carlo(A, f, N_chain, N_iter)
-
-# Сравнение решений
-print("Решение, полученное с помощью встроенной функции:", sol_scipy)
-print("Решение, полученное с помощью метода Монте-Карло:", sol_mc)
-
-# График зависимости точности решения от длины цепи Маркова
+num_chains = 100
+chain_lengths = np.arange(10000, 20000, 1000)
 errors = []
-N_iters = range(100, 2000, 100)
-for Ni in N_iters:
-    sol_mc_i = monte_carlo(A, f, N_chain, Ni)
-    error = np.linalg.norm(sol_scipy - sol_mc_i)
+for chain_length in chain_lengths:
+    monte_carlo_solve = monte_carlo_solution(size,num_chains,chain_length,a,f)
+    print(monte_carlo_solve)
+    error = np.linalg.norm(monte_carlo_solve - exact_solution)
     errors.append(error)
-plt.figure()
-plt.plot(N_iters, errors)
+
+# Построение графика
+plt.figure(figsize=(10, 6))
+plt.plot(chain_lengths, errors, marker='o')
+plt.title('Зависимость точности решения от длины цепи Маркова')
 plt.xlabel('Длина цепи Маркова')
 plt.ylabel('Ошибка')
-plt.title('Зависимость точности решения от длины цепи Маркова')
+plt.grid(True)
 plt.show()
-
-
-# # Построение графика
-# plt.figure(figsize=(10, 6))
-# plt.plot(chain_lengths, errors, marker='o')
-# plt.title('Зависимость точности решения от длины цепи Маркова')
-# plt.xlabel('Длина цепи Маркова')
-# plt.ylabel('Ошибка')
-# plt.grid(True)
-# plt.show()
 
 
